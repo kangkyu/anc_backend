f9ad44ad24fbb00038fb2c1a5228ee8f526a7186
Kang-Kyu Lee <kangkyu1111@gmail.com>
2024-03-06 23:35:47 -0800

    10 videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -23,8 +23,10 @@ class VideosController < ApplicationController
   def list_body
     @list_body ||= begin
       channel_id = "UCIsWNZwrpO_CnlaXO5Oc6bQ"
+      # anconnuri = "https://www.youtube.com/channel/UCIsWNZwrpO_CnlaXO5Oc6bQ"
+      # passionworship = "https://www.youtube.com/channel/UCBTZoebaG4rvChzKQ2D80-w"
       my_api_key = ENV["YOUR_YOUTUBE_API_KEY"]
-      url = escaped("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=#{channel_id}&maxResults=7&order=date&type=video&key=#{my_api_key}")
+      url = escaped("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=#{channel_id}&maxResults=10&order=date&type=video&key=#{my_api_key}")
       get_body(url)
     end
   end
