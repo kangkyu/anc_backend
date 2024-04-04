bb7d6ad3179957cdf6426a39c8bbe688f00b633b
Kang-Kyu Lee <kangkyu1111@gmail.com>
2024-04-03 18:33:29 -0700

    Scraping update

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -16,26 +16,26 @@ class LandingController < ApplicationController
   private
 
   def find_image_urls
-    node = Nokogiri::XML::DocumentFragment.parse(this_week_body.to_s)
-    node.css(".content-view img").collect { |tag| tag.attributes["data-src"].to_s }
-  rescue => e
-    puts e
-  end
-
-  def find_this_week_url
     node = Nokogiri::XML::DocumentFragment.parse(list_body.to_s)
-    node.css(".kboard-list-title a")[1].attributes["href"]
+    node.css(".latest_image_wrap #latest_image img").collect { |tag| tag.attributes["src"].to_s }
   rescue => e
     puts e
   end
 
-  def this_week_body
-    url = "https://anconnuri.com" + find_this_week_url.to_s
-    get_body(url)
-  end
+  # def find_this_week_url
+  #   node = Nokogiri::XML::DocumentFragment.parse(list_body.to_s)
+  #   node.css(".kboard-list-title a")[1].attributes["href"]
+  # rescue => e
+  #   puts e
+  # end
+
+  # def this_week_body
+  #   url = "https://anconnuri.com" + find_this_week_url.to_s
+  #   get_body(url)
+  # end
 
   def list_body
-    url = escaped("https://anconnuri.com/교회소식/?category1=주보&pageid=1")
+    url = escaped("https://anconnuri.com/bbs/board.php?bo_table=bulletin")
     get_body(url)
   end
 end
