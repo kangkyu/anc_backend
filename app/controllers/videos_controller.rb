7dcdca0c850b867058ad52e4103da132462595d7
Kang-Kyu Lee <kangkyu1111@gmail.com>
2024-07-12 00:10:55 -0700

    Add stale? for caching

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,12 @@
 class VideosController < ApplicationController
   def index
     @videos = ChannelVideo.order(:created_at).last(12)
-    render json: @videos
+    youtube_ids = @videos.map(&:youtube_id)
+
+    if stale?(etag: youtube_ids)
+      respond_to do |format|
+        format.json { render json: @videos }
+      end
+    end
   end
 end
