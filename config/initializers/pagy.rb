fa26a5874f74cb49de1d8851a612755dcec38cd5
Kang-Kyu Lee <kangkyu1111@gmail.com>
2024-09-28 15:36:21 -0700

    Add overflow option

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,5 +1,9 @@
 require 'pagy/extras/metadata'
 require 'pagy/extras/headers'
+require 'pagy/extras/overflow'
 
 Pagy::DEFAULT[:items] = 25  # Items per page
 Pagy::DEFAULT[:metadata] = [:page, :items, :count, :pages, :prev, :next]
+
+# Better user experience handled automatically
+Pagy::DEFAULT[:overflow] = :last_page
